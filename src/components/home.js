fb8cf47fac774c4222aea088fb783ba4be8f9f47
pheiselmann <prhmann@gmail.com>
2018-01-02 14:11:03 -0800

    initial commit

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//import './home.css';
+
+export default function Home() {
+    return (
+        <div className="home">
+            <h2>Under Construction!</h2>
+            <div className="first-message">
+              <p>Soon to be the root for my final capstone project!</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import Home from './components/home'
+import './index.css';
+
+ReactDOM.render(
+  //<App />,
+  <Home />,
+  document.getElementById('root')
+);
